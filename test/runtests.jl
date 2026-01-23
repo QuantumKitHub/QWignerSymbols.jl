c1168adec78c48cf42a00290e5a2bcae3a4f4049
Boris De Vos <boris.devos@ugent.be>
2026-01-23 17:14:52 +0100

    refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,8 @@
 using QWignerSymbols
 using Test, TestExtras
 using HalfIntegers
-include("../src/SU2k_sector.jl")
 
 @testset "QWignerSymbols.jl" verbose = true begin
-    @testset "q_combinatorics" begin
-        include("q_combinatorics.jl")
-    end
-
     @testset "q_clebsch_gordan" begin
         include("q_clebsch_gordan.jl")
     end
@@ -17,8 +12,8 @@ include("../src/SU2k_sector.jl")
     end
 end
 
-@testset "SU2k_sector" verbose = true begin
-    @testset "SU2kIrrep" begin
-        include("SU2k_sector_tests.jl")
-    end
-end
+# @testset "SU2k_sector" verbose = true begin
+#     @testset "SU2kIrrep" begin
+#         include("SU2k_sector_tests.jl")
+#     end
+# end
