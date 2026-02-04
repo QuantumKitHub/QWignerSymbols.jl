a56bb40a8074f7e05519a0c760d425aecc556f2a
Boris De Vos <boris.devos@ugent.be>
2026-02-04 15:46:44 +0100

    Revert "avoid floor shenanigans in 1.10 (maybe)"
    This reverts commit f1998fabcfed6103612e93b3b865113b871fba40.

--- test/q_sector.jl
+++ test/q_sector.jl
@@ -9,7 +9,6 @@ include(testsuite_path)
 sectorlist = (
     SU2qIrrep{1.11}, SU2qIrrep{1.3}, SU2kIrrep{2}, SU2kIrrep{3}, SU2kIrrep{4},
     SU2kIrrep{5}, SU2kIrrep{6}, SU2kIrrep{7}, SU2kIrrep{8}, SU2kIrrep{9}, SU2kIrrep{10},
-    # SU2qIrrep{convert(ComplexF64, RootOfUnity{3}())}, SU2qIrrep{1.42 + 0.4im}
 )
 
 for sector in sectorlist
